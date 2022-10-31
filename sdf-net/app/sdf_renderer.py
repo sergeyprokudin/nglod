cb4bae72e7c69069c22109bce6d17d7294601080
Sergey Prokudin <sergey.prokudin@gmail.com>
2022-10-31 17:54:21 +0100

    Update sdf_renderer.py

--- sdf-net/app/sdf_renderer.py
+++ sdf-net/app/sdf_renderer.py
@@ -97,6 +97,20 @@ if __name__ == '__main__':
 
     net.load_state_dict(torch.load(args.pretrained))
 
+    from torchinfo import summary
+    print(summary(net))
+
+    model = net
+    param_size = 0
+    for param in model.parameters():
+        param_size += param.nelement() * param.element_size()
+    buffer_size = 0
+    for buffer in model.buffers():
+        buffer_size += buffer.nelement() * buffer.element_size()
+
+    size_all_mb = (param_size + buffer_size) / 1024**2
+    print('model size: {:.3f}MB'.format(size_all_mb))
+
     net.to(device)
     net.eval()
 
